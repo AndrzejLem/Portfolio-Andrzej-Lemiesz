27d230e4e6183963589e561eb5f1ce0aba871dd7
AndrzejLem <andrzejlem1993@gmail.com>
2021-10-04 19:33:27 +0200

    90%

--- Portfolio/JS/menu.js
+++ Portfolio/JS/menu.js
@@ -34,4 +34,80 @@ function toggleMenu() {
 }
 
 
+//Form
+
+var form = document.querySelector(".my-form"),
+    fields = document.querySelectorAll("[data-error");
+
+    function isNotEmpty(field){
+        return field.value !== "";
+    }
+
+    function isAtLeast(field, min){
+        return field.value.length > min;
+    }
+
+    function displayErrors(errors){
+        var ul = document.querySelector("ul.errors");
+
+        if(!ul){
+            ul = document.createElement("ul");
+            ul.classList.add("errors");
+        }
+
+        ul.innerHTML = "";
+
+        errors.forEach(function(errors){
+            var li = document.createElement("li");
+
+            li.textContent = errors;
+
+            ul.appendChild(li);
+        });
+
+        form.parentNode.insertBefore(ul, form);
+
+    }
+
+    form.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        var errors = [],
+            isValid = false;
+
+        for(var i = 0; i < fields.length; i++) {
+
+            var field = fields[i];
+
+            if(field.type === "text"){
+                isValid = isNotEmpty(field);
+            } else if(field.type === "email"){
+                if(field.value === ""){
+                    isValid = isNotEmpty(field);
+                }
+            } else if(field.type === "textarea"){
+                isValid = isAtLeast(field, 10);
+            }
+
+            if(!isValid){
+                field.classList.add("error")
+                errors.push(field.dataset.error);
+            } else{
+                field.classList.remove("error");
+            }
+
+        }
+
+        if(errors.length){
+            displayErrors(errors);
+        } else{
+            form.submit();
+        }
+
+        console.log(errors);
+        
+    }, false);
+
+
 })();
